d9334ac00e29dbc388c04f1ca1dd07eadb4c2a37
SHVNKRST <shivankrst@gmail.com>
2024-11-22 17:13:14 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: '0101EX211003',
+  title: '0101AU211060',
   description: 'API Frontend Interface',
 };
 
